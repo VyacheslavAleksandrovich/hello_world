3fd7a9116355aa13e8ef0aba47c1c529d55b1bc9
Vyacheslav <vyacheslav@book.home>
2017-07-31 01:21:01 +1100

    main.c hange

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 int main(){
 int a = 100;
 int b = 0;
@@ -5,8 +6,8 @@ bool flag = getFlag();
 while(a > b) {
 if (flag) {
 b += 2;
-for (int i = 0; i < calcCPU(a,b); i++) {
-int grep = initGrep(i);
+for (int i = 0; i <= calcCPU(a,b); i++) {
+int grep = initGrep(i+1);
 b += grep%2;
 }
 std::cout << a;
@@ -20,7 +21,7 @@ if (a%3 == 0) {
 std::cout << a;
 }
 else {
-a -= 3;
+a -= 2;
 std::cout << b;
 }
 }
